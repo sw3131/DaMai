49aae7221e7bacde393175af33fe9240c90e4c19
sw3131 <sw3131@163.com>
2024-10-31 23:11:31 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # 设置页面标题
-st.title("月环比分析工具")
+st.title("大麦-数据与策略-月环比智能")
 
 # 文件上传
 uploaded_file = st.file_uploader("上传Excel文件", type=["xlsx"])
@@ -16,17 +16,27 @@ if uploaded_file is not None:
 
     results = {}
 
-    # 客户维度分析，包含BD列
+    # 客户维度分析，包括BD列
     if 'BD' in df.columns:
         monthly_data_bd = df.groupby(['BD', '月份']).agg({'实付金额': 'sum'}).reset_index()
         comparison_bd = monthly_data_bd[monthly_data_bd['月份'].isin([latest_month, latest_month - 1])]
 
         if comparison_bd.shape[0] >= 2:
             pivot_table_bd = comparison_bd.pivot(index='BD', columns='月份', values='实付金额')
-            pivot_table_bd['环比'] = (pivot_table_bd[latest_month] - pivot_table_bd[latest_month - 1]) / pivot_table_bd[
-                latest_month - 1] * 100
+            pivot_table_bd['环比'] = (pivot_table_bd[latest_month] - pivot_table_bd[latest_month - 1]) / pivot_table_bd[latest_month - 1] * 100
             pivot_table_bd = pivot_table_bd.reset_index().sort_values(by='环比', ascending=False)
-            results['客户维度分析'] = pivot_table_bd
+            results['BD维度分析'] = pivot_table_bd
+
+    # 客户维度分析
+    if 'BD' in df.columns:
+        monthly_data_customer = df.groupby(['客户名称', 'BD', '月份']).agg({'实付金额': 'sum'}).reset_index()
+        comparison_customer = monthly_data_customer[monthly_data_customer['月份'].isin([latest_month, latest_month - 1])]
+
+        if comparison_customer.shape[0] >= 2:
+            pivot_table_customer = comparison_customer.pivot(index=['客户名称', 'BD'], columns='月份', values='实付金额')
+            pivot_table_customer['环比'] = (pivot_table_customer[latest_month] - pivot_table_customer[latest_month - 1]) / pivot_table_customer[latest_month - 1] * 100
+            pivot_table_customer = pivot_table_customer.reset_index().sort_values(by='环比', ascending=False)
+            results['客户维度分析'] = pivot_table_customer
 
     # 其他维度分析
     for column_name in ['商品名称', '主营类型', '商品分类', '订单类型']:
@@ -36,8 +46,7 @@ if uploaded_file is not None:
 
             if comparison.shape[0] >= 2:
                 pivot_table = comparison.pivot(index=column_name, columns='月份', values='实付金额')
-                pivot_table['环比'] = (pivot_table[latest_month] - pivot_table[latest_month - 1]) / pivot_table[
-                    latest_month - 1] * 100
+                pivot_table['环比'] = (pivot_table[latest_month] - pivot_table[latest_month - 1]) / pivot_table[latest_month - 1] * 100
                 pivot_table = pivot_table.reset_index().sort_values(by='环比', ascending=False)
                 results[column_name] = pivot_table
 
